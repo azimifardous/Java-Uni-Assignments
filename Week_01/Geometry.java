910b7937438c0e886f29f70795e316b19ddd01bc
Ahmad Fardous Azimi <101262122+azimifardous@users.noreply.github.com>
2022-10-07 10:28:33 +0300

    Exercise 02

---
+++ Week_01/Geometry.java
@@ -0,0 +1,14 @@
+public class Geometry {
+    public static void main(String[] args) {
+        // Print the number of sides several geometric shapes
+
+        int sides = 7; // declaration with initialization
+        System.out.println("A heptagon has " + sides + " sides.");
+
+        sides = 10; // assignment statements
+        System.out.println("A decagon has " + sides + " sides.");
+
+        sides = 12;
+        System.out.println("A dedacagon has " + sides + " sides.");
+    }
+}
